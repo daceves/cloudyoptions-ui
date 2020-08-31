3f4e458801dc2a06a050527375142a263818394d
Tristan Wells <tristanwells@Tristans-MacBook-Pro.local>
2020-08-31 11:38:57 -0700

    changed url to http, created theme and implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
+import {ThemeProvider} from '@material-ui/core/styles'
+import { createMuiTheme } from '@material-ui/core/styles';
 import AppBarTop from './AppBarSection/AppBarTop';
 import './App.css';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#3D2809'
+    },
+    secondary: {
+      main: '#1A8A40'
+    },
+  }
+})
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <meta name="google-signin-client_id" content="740164561913-sjupbaofgfgtfhsm5ggo6lnqj38akrcm.apps.googleusercontent.com"/>
-        <AppBarTop/>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <header className="App-header">
+          <meta name="google-signin-client_id" content="740164561913-sjupbaofgfgtfhsm5ggo6lnqj38akrcm.apps.googleusercontent.com"/>
+          <AppBarTop/>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/LoginSection/LoginAPI.ts
+++ src/LoginSection/LoginAPI.ts
@@ -2,14 +2,8 @@ import axios  from 'axios'
 
 export async function SignInRequest (googleIdToken : string) {
     try {
-        const response = await axios.post('https://localhost:4000/api/auth/signin',{
+        const response = await axios.post('http://localhost:4000/api/auth/signin',{
             tokenid: googleIdToken,
-        },
-        {
-            headers: {
-                'Content-Type': 'application/json',
-                
-            }
         })
         .then((response)=> {
             console.log(response);
